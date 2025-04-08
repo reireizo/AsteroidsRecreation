d240f5624cb62f74d291bea34db5f1b9ab461f0d
reireizo <nathan.irwin2021@gmail.com>
2025-04-08 18:44:37 -0400

    Final Notes Cleanup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,33 +7,55 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // Reference to the player object.
     public Player player;
+    // Reference to the explosion particle system object.
     public ParticleSystem explosionEffect;
+    // Reference to the Score UI object.
     public TextMeshProUGUI scoreUI;
+    // Reference to the Final Score UI object.
     public TextMeshProUGUI finalScoreUI;
+    // Reference to the Hi-Score UI object.
     public TextMeshProUGUI hiScoreUI;
+    // Reference to the Game Over UI objects parent.
     public GameObject GameOverUI;
+    // Reference to the Lives Icon UI images.
     public Image[] livesUI;
+
+    // Int value representing the number of lives the player has.
     public int lives = 3;
+    // Int value representing the player's current score.
     public int score = 0;
+    // Int value representing the player's Hi-Score.
     public int hiScore = 0;
+    // Float value representing how long it takes for the player to respawn after dying.
     public float respawnTime = 3.0f;
+    // Float value representing how long the player remains invincible after respawning.
     public float invincibleTime = 3.0f;
 
+    // Bool value representing if the game is in the Game Over state.
     bool isGameOver;
 
+    // OnEnable needs to:
+    // > Subscribe to the player's HasDied action, to call PlayerDied.
+    // > Subscribe to the asteroids' HasBeenDestroyed action, to call AsteroidDestroyed.
     void OnEnable()
     {
         Player.HasDied += PlayerDied;
         Asteroid.HasBeenDestoryed += AsteroidDestroyed;
     }
 
+    // OnEnable needs to:
+    // > Unsubscribe to the player's HasDied action.
+    // > Subscribe to the asteroids' HasBeenDestroyed action.
     void OnDisable()
     {
         Player.HasDied -= PlayerDied;
         Asteroid.HasBeenDestoryed -= AsteroidDestroyed;
     }
 
+    // Start needs to:
+    // > Get hiScore from PlayerPrefs if it is present.
     void Start()
     {
         if (PlayerPrefs.HasKey("hiScore"))
@@ -42,6 +64,8 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // Update needs to:
+    // > Get input when the game is over to reset the game.
     void Update()
     {
         if (isGameOver)
@@ -54,6 +78,14 @@ public class GameManager : MonoBehaviour
         
     }
 
+    // AsteroidDestroyed needs to:
+    // > Pass in the asteroid being destroyed.
+    // > Move and call the explosion effect object to play the explosion where the asteroid was destroyed.
+    // > Award points based on Asteroid size.
+    //   > If between min-0.75: 100 points (hard to hit)
+    //   > If between 0.75-1.2: 50 points (about average)
+    //   > if between 1.2-max: 25 points (easy target)
+    // > Update the score UI with the new score.
     public void AsteroidDestroyed(Asteroid asteroid)
     {
         explosionEffect.transform.position = asteroid.transform.position;
@@ -75,6 +107,12 @@ public class GameManager : MonoBehaviour
         UpdateScoreUI();
     }
 
+    // PlayerDied needs to:
+    // > Move and call the explosion effect to play where the player was.
+    // > Decrement the lives count.
+    // > Update the Lives UI using the new lives count.
+    // > When lives are at 0 or less, call the Game Over function.
+    // > If not, respawn the player after respawnTime.
     public void PlayerDied()
     {
         explosionEffect.transform.position = player.transform.position;
@@ -92,6 +130,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // Respawn needs to:
+    // > Put the player back at the origin.
+    // > Put the player on the Invincibility layer (to not die immediately if you respawn on an asteroid).
+    // > Re-activate the player object.
+    // > Put the player back on the normal Player layer after invincibileTime.
     void Respawn()
     {
         //Debug.Log("I died!");
@@ -101,11 +144,20 @@ public class GameManager : MonoBehaviour
         Invoke(nameof(TurnOnCollisions), invincibleTime);
     }
 
+    // TurnOnCollisions needs to:
+    // > Put the player back on the normal Player layer.
     void TurnOnCollisions()
     {
         player.gameObject.layer = LayerMask.NameToLayer("Player");
     }
 
+    // GameOver needs to:
+    // > Activate the isGameOver bool.
+    // > Hide the normal Score UI.
+    // > Activate the Game Over UI.
+    // > Save the Hi-Score.
+    // > Update the Final Score UI.
+    // > Update the Hi-Score UI.
     void GameOver()
     {
         isGameOver = true;
@@ -116,22 +168,40 @@ public class GameManager : MonoBehaviour
         UpdateHiScoreUI();
     }
 
+    // UpdateScoreUI needs to:
+    // > Update the score UI text with the current score.
     void UpdateScoreUI()
     {
         scoreUI.SetText(score.ToString());
     }
+
+    // UpdateLivesUI needs to:
+    // > Pass in the new lives count.
+    // > Use that new count to access the index of the icon that needs to be disabled, and disable it.
     void UpdateLivesUI(int newLives)
     {
         livesUI[newLives].enabled = false;
     }
+
+    // UpdateFinalScoreUI needs to:
+    // > Update the final score UI with the current score.
     void UpdateFinalScoreUI()
     {
         finalScoreUI.SetText("Final Score: " + score.ToString());
     }
+
+    // UpdateHiScoreUI needs to:
+    // > Update the hi-score UI with the current hi-score.
     void UpdateHiScoreUI()
     {
         hiScoreUI.SetText("Hi-Score: " + hiScore.ToString());
     }
+
+    // SaveHiScore needs to:
+    // > Check if score is greater than hi-score.
+    // > If so:
+    //   > Set Hi-Score to be current score.
+    //   > Save Hi-Score to PlayerPrefs under hiScore.
     void SaveHiScore()
     {
         if (score > hiScore)
